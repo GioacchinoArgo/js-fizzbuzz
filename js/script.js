ac913b67bf6c7820274cbf3ee89a988847301010
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-20 19:17:02 +0100

    added content of javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK')
+
+let message = '';
+
+for (let i = 1; i <= 100; i++) {
+console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        message = 'FizzBuzz';
+        console.log (message)
+    } else if (i % 3 === 0) {
+        message = 'fizz';
+        console.log(message)
+    } else if (i % 5 === 0) {
+        message = 'buzz';
+        console.log(message)
+    }
+
+}
