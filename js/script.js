c187383799aa9f2d8587c3af561675b3e649f644
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-27 15:28:53 +0100

    fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 console.log('JS OK')
 
-let message = '';
 
 for (let i = 1; i <= 100; i++) {
-console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
-        message = 'FizzBuzz';
-        console.log (message)
+        console.log ('FizzBuzz')
     } else if (i % 3 === 0) {
-        message = 'fizz';
-        console.log(message)
+        console.log('fizz')
     } else if (i % 5 === 0) {
-        message = 'buzz';
-        console.log(message)
+        console.log('buzz')
+    } else {
+        console.log(i);
     }
+    
 
 }
